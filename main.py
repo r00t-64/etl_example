6eee163653e3488f0f910ba81761f4aeb3862864
Isaac Alexis Rivera Sanchez <ariveras@uni.pe>
2023-06-14 01:55:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+from sqlalchemy import create_engine
+import pyodbc
+import pandas as pd
+import os
+
+#get passwords from environment var
+pwd = os.environ['PGPASS']
+uid = os.environ['PGUID']
+# sql db details
+driver = '{SQL Server Native Client 11.0}'
+server = 'haq-PC'
+database  = 'AdventureWorksDW2019'
+
+#extract data from sql server
+def extract():
+  try:
+      src_conn =  pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + '\SQLEXPRESS'+ ';DATABASE=' + ';UID=' + uid  + ';PWD=' +pwd) 
+      sql_cursor = src_conn.cursor()
+      
+      src_cursor.execute("""  select t.name as table_name
+          from sys.tables t where t.name in ('DisProduct', ' DisProductSubcategory , ''
+      """)
+      src_tables =  src_cursor.fetchall()
+      
+  except Exception as e:
+      
+  finally:
+      
 
